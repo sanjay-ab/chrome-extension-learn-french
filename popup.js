c9c399109e8b74893aae21792998345b890ce190
Sanjay Booshanam <s2556413@ed.ac.uk>
2024-09-04 11:08:22 +0100

    small refactor and docs update

--- popup.js
+++ popup.js
@@ -1,62 +1,61 @@
 
-const frtxt = document.getElementById('frtxt'); //french text holder
-const entxt = document.getElementById('entxt'); //english text holder
-const type = document.getElementById('type'); //holder for type of word
-const prev = document.getElementById('prev'); //button to move to previous word
-const repeat = document.getElementById('repeat'); //button to repeat the word
-const next = document.getElementById('next'); //button to move to next word
-const url = chrome.runtime.getURL("data.json"); //get the data required 
+const frtxt = document.getElementById('frtxt');  // french text holder
+const entxt = document.getElementById('entxt');  // english text holder
+const type = document.getElementById('type');  // holder for type of word (e.g. noun, verb, etc.)
+const prev = document.getElementById('prev');  // button to move to previous word
+const repeat = document.getElementById('repeat');  // button to repeat the word (using tts)
+const next = document.getElementById('next');  // button to move to next word
+const url = chrome.runtime.getURL("data.json");  // get the data required 
 
 fetch(url)
 	.then((response) => response.json())
 	.then(function(json){
-		let numbers = []; //array to hold the ids of the list of words we've gone through
-		let pointer =0 //shows where in the list we are
-		let number = Math.floor(Math.random()*758); //choose random id for random word
-		let eng = json[String(number)+"."].english; //set english translation
-		numbers.push(number); //add id to list
-		frtxt.innerHTML = json[String(number)+ "."].french; //set french label to word
-		type.innerHTML = json[String(number)+"."].type; //set type to type of word e.g. noun, verb, etc.
-		chrome.tts.speak(frtxt.innerHTML, {'lang':'fr','rate':1.0, 'volume': 0.25}); //get chrome to say word with tts
+		let prevWordIds = []; 
+		let locationInList = 0
+		let nextWordId = Math.floor(Math.random()*758);
+		let eng = json[String(nextWordId)+"."].english; 
+		prevWordIds.push(nextWordId);
+		frtxt.innerHTML = json[String(nextWordId)+ "."].french;
+		type.innerHTML = json[String(nextWordId)+"."].type;
+		chrome.tts.speak(frtxt.innerHTML, {'lang':'fr','rate':1.0, 'volume': 0.25});
 
-		repeat.onclick = function(){ //repeat saying the word 
+		repeat.onclick = function(){
 			chrome.tts.speak(frtxt.innerHTML, {'lang':'fr','rate':1.0, 'volume': 0.25});
 		}
 
-		next.onclick = function(){ //move to next word
-			if (pointer<(numbers.length-1)){ //change pointer to correct index
-				pointer += 1;
-				number = numbers[pointer];
+		next.onclick = function(){
+			if (locationInList < (prevWordIds.length-1)){
+				// if pointer is not at the end of the list, move to next word
+				locationInList += 1;
+				nextWordId = prevWordIds[locationInList];
 			} else {
-				while(numbers.includes(number)){
-					number = Math.floor(Math.random()*758); //choose new random id for next word
+				// if pointer is at the end of the list, generate a new word
+				while(prevWordIds.includes(nextWordId)){
+					nextWordId = Math.floor(Math.random()*758);
 				}
-				numbers.push(number);
-				pointer += 1
+				prevWordIds.push(nextWordId);
+				locationInList += 1
 			}
-			frtxt.innerHTML = json[String(number)+ "."].french; //set values for different labels
+			frtxt.innerHTML = json[String(nextWordId)+ "."].french;
 			entxt.innerHTML = "Click to reveal translation";
-			eng = json[String(number)+"."].english;
-			type.innerHTML = json[String(number)+"."].type;
+			eng = json[String(nextWordId)+"."].english;
+			type.innerHTML = json[String(nextWordId)+"."].type;
 			chrome.tts.speak(frtxt.innerHTML, {'lang':'fr','rate':1.0, 'volume': 0.25});
 		}
 
-		prev.onclick = function(){ //move back to previous words
-			if (pointer){ //check if pointer is at start of list 
-				pointer = pointer -1 
+		prev.onclick = function(){
+			if (locationInList){  // check if pointer is at start of list 
+				locationInList = locationInList -1 
 			} 
-			number = numbers[pointer]; //get id of previous word
-			frtxt.innerHTML = json[String(number)+ "."].french; //set labels
+			nextWordId = prevWordIds[locationInList];
+			frtxt.innerHTML = json[String(nextWordId)+ "."].french;
 			entxt.innerHTML = "Click to reveal translation";
-			eng = json[String(number)+"."].english;
-			type.innerHTML = json[String(number)+"."].type;
+			eng = json[String(nextWordId)+"."].english;
+			type.innerHTML = json[String(nextWordId)+"."].type;
 			chrome.tts.speak(frtxt.innerHTML, {'lang':'fr','rate':1.0, 'volume': 0.25});
 		}
 
-		entxt.onclick = function(){ //when clicked the english translation is revealed
+		entxt.onclick = function(){  // when clicked the english translation is revealed
 			entxt.innerHTML = eng; 
 		}
-
-
-
 	})
